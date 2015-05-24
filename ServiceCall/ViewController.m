0dcbb2a5aa359c90d585b3c4b6b3035373c20886
Faisal Syed <Faisal_Syed@Faisals-Mac-mini.local>
2015-05-24 09:41:59 -0700

    Made the basic service call and fixed bug in which you couldn't search for a restaurant with a "two word" name

--- ServiceCall/ViewController.m
+++ ServiceCall/ViewController.m
@@ -16,7 +16,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //APP_ID = 3b2ac1cc
+    
+    //APP_KEY = 37793f2d4901156ec5f50a51ea4f594d
+    
+    //Service Call...
+    NSString *userEnteredString = @"Pizza Hut";
+    NSString *correctedString = [userEnteredString stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
+    
+    NSString *urlString = [NSString stringWithFormat:@"https://api.nutritionix.com/v1_1/search/%@?results=0%%3A20&cal_min=0&cal_max=50000&fields=item_name%%2Cbrand_name%%2Citem_id%%2Cbrand_id&appId=3b2ac1cc&appKey=37793f2d4901156ec5f50a51ea4f594d", correctedString];
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url
+                                                cachePolicy:NSURLRequestReturnCacheDataElseLoad
+                                            timeoutInterval:30];
+    
+    NSData *urlData;
+    NSURLResponse *response;
+    NSError *error;
+    
+    
+    urlData = [NSURLConnection sendSynchronousRequest:urlRequest
+                                    returningResponse:&response
+                                                error:&error];
+    
+    NSMutableArray *myArray = [NSJSONSerialization JSONObjectWithData:urlData options:kNilOptions error:&error];
+    
+    NSLog(@"%@", myArray);
+    
+    
+
+   
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
