8610080d6940df6c6131f41fd6c60fae2fe7238d
Faisal Syed <Faisal_Syed@Faisals-Mac-mini.local>
2015-05-24 09:02:27 -0700

    Initial Commit

---
+++ ServiceCall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ServiceCall
+//
+//  Created by Faisal Syed on 5/24/15.
+//  Copyright (c) 2015 Faisal Syed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ServiceCallTests/ServiceCallTests.m
@@ -0,0 +1,40 @@
+//
+//  ServiceCallTests.m
+//  ServiceCallTests
+//
+//  Created by Faisal Syed on 5/24/15.
+//  Copyright (c) 2015 Faisal Syed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ServiceCallTests : XCTestCase
+
+@end
+
+@implementation ServiceCallTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
